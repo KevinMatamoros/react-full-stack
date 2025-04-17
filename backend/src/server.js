b76431bbc2fe000e83c0246526ef46fbb08433de
Matamoros-Serrano, Kevin <matamorosk@ae.com>
2025-04-17 12:52:11 -0600

    Issue getting pproject_id

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,16 +9,16 @@ const __dirname = path.dirname(__filename);
 
 const credentials = {
   type: "service_account",
-  project_id: process.env.GOOGLE_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_AUTH_URI,
-  token_uri: process.env.GOOGLE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  project_id: String(process.env.GOOGLE_PROJECT_ID),
+  private_key_id: String(process.env.GOOGLE_PRIVATE_KEY_ID),
+  private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(/\\n/g, '\n'),
+  client_email: String(process.env.GOOGLE_CLIENT_EMAIL),
+  client_id: String(process.env.GOOGLE_CLIENT_ID),
+  auth_uri: String(process.env.GOOGLE_AUTH_URI),
+  token_uri: String(process.env.GOOGLE_TOKEN_URI),
+  auth_provider_x509_cert_url: String(process.env.GOOGLE_AUTH_PROVIDER_CERT_URL),
+  client_x509_cert_url: String(process.env.GOOGLE_CLIENT_CERT_URL),
+  universe_domain: String(process.env.GOOGLE_UNIVERSE_DOMAIN)
 };
 
 admin.initializeApp({
